419678509f582b9762695abc8ef6b42cbc10fecb
Gowtham <gowthamsaravan1@gmail.com>
2021-08-20 11:47:29 +0530

    added address file

---
+++ Address.swift
@@ -0,0 +1,12 @@
+//
+//  Address.swift
+//  Dashboard
+//
+//  Created by Gowtham S on 20/08/21.
+//
+
+import Foundation
+
+struct Address {
+   let a = "RM2MR746UHwgJWuPo3vjMiwgJkR9DFaZUD"// you wallet address goes here
+}
